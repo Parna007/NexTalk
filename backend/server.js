aa97fba33f3ec60117c885254406d6d38245e49a
parna das <kathikadad12345@gmai.com>
2025-07-14 14:43:53 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import authRoutes from './src/routes/auth.route.js';
+import dotenv from 'dotenv';
+import { connectDB } from './src/lib/db.js';
+
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
-    console.log("server is running on 5001");
-})
+    console.log("server is running on PORT:" + PORT);
+    connectDB();
+})  
