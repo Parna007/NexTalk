7f6fa017501def0e3e347faff9b4e5ff16974098
parna das <kathikadad12345@gmai.com>
2025-07-17 20:01:19 +0530

    backend final code

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,32 @@ import messageRoutes from './src/routes/message.route.js';
 import dotenv from 'dotenv';
 import { connectDB } from './src/lib/db.js';
 import cookieParser from 'cookie-parser';
+import cors from "cors";
+import { app, server } from './src/lib/socket.js';
 
 
-const app = express();
+//const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.listen(5001, () => {
+// app.listen(5001, () => {
+//     console.log("server is running on PORT:" + PORT);
+//     connectDB();
+// })  
+
+server.listen(5001, () => {
     console.log("server is running on PORT:" + PORT);
     connectDB();
 })  
