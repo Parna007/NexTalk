6c011bab752da71869151a7c79e5ea50caa88f0e
parna das <kathikadad12345@gmai.com>
2025-07-17 14:43:51 +0530

    message routes created

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import authRoutes from './src/routes/auth.route.js';
+import messageRoutes from './src/routes/message.route.js';
 import dotenv from 'dotenv';
 import { connectDB } from './src/lib/db.js';
 import cookieParser from 'cookie-parser';
@@ -13,6 +14,7 @@ app.use(cookieParser());
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(5001, () => {
     console.log("server is running on PORT:" + PORT);
